9c823818573d22b097dd48a1fabb3bcb9f7a3e14
raaffs <suyash.saraf@somaiya.edu>
2025-12-20 07:04:01 +0530

    updated profiles and users to use timestamptz and add phone_number to profiles

--- server/internal/models/models.go
+++ server/internal/models/models.go
@@ -13,20 +13,21 @@ type User struct {
     ID           int       `json:"id"`
     Email        string    `json:"email"`
     Username     string    `json:"username"`
-    PasswordHash string    `json:"password_hash"`
+    PasswordHash string    `json:"-"`
     CreatedAt    time.Time `json:"created_at"`
     UpdatedAt    time.Time `json:"updated_at"`
 }
 
 // Profiles table equivalent
 type Profile struct {
-    ID           int       `json:"id"`
-    UserID       int       `json:"user_id"`
-    FullName     string    `json:"full_name"`
-    DateOfBirth  time.Time `json:"date_of_birth"`
-    AadhaarNumber string   `json:"aadhaar_number"`
-    PhoneNumber     string    `json:"phone_number"`
-    Address      string    `json:"address,omitempty"` // omitempty if null
-    CreatedAt    time.Time `json:"created_at"`
-    UpdatedAt    time.Time `json:"updated_at"`
+    ID            int       `json:"id"`
+    UserID        int       `json:"user_id"`
+    FullName      string    `json:"full_name"`
+    DateOfBirth   time.Time `json:"date_of_birth"`
+    AadhaarNumber string    `json:"aadhaar_number"`
+    UniqueID      string    `json:"unique_id"`    
+    PhoneNumber   string    `json:"phone_number"`
+    Address       string    `json:"address"`     
+    CreatedAt     time.Time `json:"created_at"`
+    UpdatedAt     time.Time `json:"updated_at"`
 }
